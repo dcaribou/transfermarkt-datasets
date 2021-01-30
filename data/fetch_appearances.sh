a8b0303e1eabebdfd3a5a76217277397b0f377fc
dcereijodo <dcereijo@digitalorigin.com>
2021-01-30 17:56:46 +0100

    Pin scraper version

--- data/fetch_appearances.sh
+++ data/fetch_appearances.sh
@@ -1,5 +1,7 @@
 set -xe
 
+scraper_version=v0.0.1
+
 # relative paths
 site_map_file=$1
 output_file=$2
@@ -9,7 +11,7 @@ output_file=$PWD/$output_file
 
 docker run \
   -v "$(pwd)"/$site_map_file:/app/site_map.json \
-  dcaribou/transfermarkt-scraper:latest \
+  dcaribou/transfermarkt-scraper:$scraper_version \
   scrapy crawl partial -a site_map_file=site_map.json \
   > $output_file
 
