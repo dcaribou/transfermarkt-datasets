562bdb93c612db61ef75aa8c2a75bf511312be31
dcaribou <davidcereijo@live.com>
2022-03-18 17:21:36 +0100

    debug

--- prepare_on_batch.sh
+++ prepare_on_batch.sh
@@ -18,6 +18,7 @@ prep_status=0 # TODO: remove this line
 if [ $prep_status == 0 ]; then
   eval `ssh-agent -s`
   ssh-add - <<< $(aws --region eu-west-1 secretsmanager get-secret-value --secret-id /ssh/transfermarkt-datasets/deploy-keys | jq -r '.SecretString')
+  cat /root/.ssh/known_hosts # TODO: remove this line
   dvc commit -f && git add data && git commit -m 'Prepared' && git push -u origin $BRANCH
 fi
 
