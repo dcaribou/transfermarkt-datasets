3e74aebd055e93e611d42a8c690b817742212af2
dcaribou <davidcereijo@live.com>
2022-03-27 18:30:47 +0200

    refactor scripts

--- 2_prepare.py
+++ 2_prepare.py
@@ -1,8 +1,22 @@
-"""Bulid prepared datasets in data/prep using raw data in data/raw.
+"""
+Bulid prepared datasets in data/prep using raw data in data/raw.
+
+usage: 2_prepare.py [-h] {local,cloud} ...
+
+positional arguments:
+  {local,cloud}
+    local        Run the acquiring step locally
+    cloud        Run the acquiring step in the cloud
+
+optional arguments:
+  -h, --help     show this help message and exit
+  
 """
 import os
 from prep.asset_runner import AssetRunner
 import argparse
+
+from cloud_lib import submit_batch_job_and_wait
 
 def fail_if_invalid(asset_runner):
   if not asset_runner.validate_resources():
@@ -10,30 +24,77 @@ def fail_if_invalid(asset_runner):
   else:
     print("All good \N{winking face}")
 
+
+def prepare_on_local(raw_files_location, refresh_metadata, run_validations, season, func):
+
+  runner = AssetRunner(raw_files_location, season)
+
+  if refresh_metadata:
+    # generate frictionless data package for prepared assets
+    runner.generate_datapackage('data/prep')
+  elif run_validations:
+    # run data package validations
+    fail_if_invalid(runner)
+  else:
+    # generate prepared data assets in 'stage' folder
+    runner.process_assets()
+    runner.generate_datapackage()
+    fail_if_invalid(runner)
+    os.system("cp prep/stage/* data/prep")
+
+
+def prepare_on_cloud(job_name, job_queue, job_definition, branch, args, func):
+
+  submit_batch_job_and_wait(
+    job_name=job_name,
+    job_queue=job_queue,
+    job_definition=job_definition,
+    branch=branch,
+    script="2_prepare.py",
+    args=args,
+    vcpus=2,
+    memory=9216
+  )
+
+# main
+
 parser = argparse.ArgumentParser()
-parser.add_argument('--raw-files-location', required=False, default='data/raw')
-parser.add_argument('--refresh-metadata', action='store_const', const=True, required=False, default=False)
-parser.add_argument('--run-validations', action='store_const', const=True, required=False, default=False)
-parser.add_argument('--season', required=False)
 
-args = parser.parse_args()
+subparsers = parser.add_subparsers()
 
-raw_files_location = args.raw_files_location # ../data/raw
-refresh_metadata = args.refresh_metadata
-run_validations = args.run_validations
-season = args.season
+local_parser = subparsers.add_parser('local', help='Run the acquiring step locally')
+local_parser.add_argument('--raw-files-location', required=False, default='data/raw')
+local_parser.add_argument('--refresh-metadata', action='store_const', const=True, required=False, default=False)
+local_parser.add_argument('--run-validations', action='store_const', const=True, required=False, default=False)
+local_parser.add_argument('--season', required=False)
 
-runner = AssetRunner(raw_files_location, season)
+local_parser.set_defaults(func=prepare_on_local)
 
-if refresh_metadata:
-  # generate frictionless data package for prepared assets
-  runner.generate_datapackage('data/prep')
-elif run_validations:
-  # run data package validations
-  fail_if_invalid(runner)
-else:
-  # generate prepared data assets in 'stage' folder
-  runner.process_assets()
-  runner.generate_datapackage()
-  fail_if_invalid(runner)
-  os.system("cp prep/stage/* data/prep")
+cloud_parser = subparsers.add_parser('cloud', help='Run the acquiring step in the cloud')
+cloud_parser.add_argument(
+  '--job-name',
+  default="on-cli"
+)
+cloud_parser.add_argument(
+  '--job-queue',
+  default="transfermarkt-datasets-batch-compute-job-queue"
+)
+cloud_parser.add_argument(
+  '--job-definition',
+  default="transfermarkt-datasets-batch-job-definition-dev"
+)
+cloud_parser.add_argument(
+  '--branch',
+  required=True
+)
+cloud_parser.add_argument(
+  "args",
+  default=["--asset", "all", "--season", "2021"],
+  nargs="*"
+)
+cloud_parser.set_defaults(func=prepare_on_cloud)
+
+arguments = parser.parse_args()
+arguments.func(**vars(arguments))
+
+
--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 PLATFORM = linux/arm64 # linux/amd64
 BRANCH = $(shell git rev-parse --abbrev-ref HEAD)
+JOB_NAME = on-cli
 
 build : 
 	docker build --platform=$(PLATFORM) -t dcaribou/transfermarkt-datasets:dev .
@@ -7,48 +8,33 @@ build :
 push :
 	docker push dcaribou/transfermarkt-datasets:dev
 
-COMMAND = 2_prepare.py
-EXEC_COMMAND = $(shell PASSED_COMMAND="$(COMMAND)"; echo $${PASSED_COMMAND// /,})
-
-run_local :
-	docker run -ti \
-		--env-file .env \
-		-v `pwd`/.:/app/transfermarkt-datasets/ \
-		--memory=4g  \
-		dcaribou/transfermarkt-datasets:dev $(BRANCH) $(COMMAND)
-
 run_bootstrap :
 	docker run -ti \
 		--env-file .env \
 		--memory=4g  \
-		dcaribou/transfermarkt-datasets:dev $(BRANCH) $(COMMAND)
+		dcaribou/transfermarkt-datasets:dev $(BRANCH) $(ARGS)
 
-run_batch : JOB_DEFINITION_NAME = transfermarkt-datasets-batch-job-definition-dev
-run_batch : REVISION = $(shell \
-  aws batch describe-job-definitions | \
-  jq --arg jdname $(JOB_DEFINITION_NAME) '.jobDefinitions | map(select(.jobDefinitionName==$$jdname)) | sort_by(.revision) | last | .revision' \
-)
+acquire_local :
+	python 1_acquire.py local $(ARGS)
+acquire_docker : 
+	docker run -ti \
+			--env-file .env \
+			-v `pwd`/.:/app/transfermarkt-datasets/ \
+			--memory=4g  \
+			dcaribou/transfermarkt-datasets:dev \
+				python 1_acquire.py local $(ARGS)
+acquire_cloud : JOB_DEFINITION_NAME = transfermarkt-datasets-batch-job-definition-dev
+acquire_cloud :
+	python 1_acquire.py cloud --branch $(BRANCH) --job-name $(JOB_NAME) "$(ARGS)"
 
-run_batch : JOB_NAME = on-cli
-run_batch : JOB_ID = $(shell \
-	aws batch submit-job \
-		--job-name $(JOB_NAME) \
-		--job-queue transfermarkt-datasets-batch-compute-job-queue \
-		--job-definition $(JOB_DEFINITION_NAME):$(REVISION) \
-		--container-overrides \
-			command=$(BRANCH),$(EXEC_COMMAND) \
-	| jq -r '.jobId' \
-)
-run_batch:
-	JOB_ID=$(JOB_ID); \
-	JOB_STATUS=$$(aws batch describe-jobs --jobs $$JOB_ID | jq -r '.jobs[0].status'); \
-	while [ $$JOB_STATUS != FAILED ] && [ $$JOB_STATUS != SUCCEEDED ]; do \
-		sleep 5; \
-		JOB_STATUS=$$(aws batch describe-jobs --jobs $$JOB_ID | jq -r '.jobs[0].status'); \
-		echo $$JOB_STATUS; \
-	done; \
-	if [ $$JOB_STATUS = FAILED ]; then \
-		exit 1; \
-	else \
-		exit 0; \
-	fi
+prepare_local :
+	python 2_prepare.py local $(ARGS)
+prepare_docker :
+	docker run -ti \
+			--env-file .env \
+			-v `pwd`/.:/app/transfermarkt-datasets/ \
+			--memory=4g  \
+			dcaribou/transfermarkt-datasets:dev \
+				python 2_prepare.py local $(ARGS)
+prepare_cloud : 
+	python 2_prepare.py cloud --branch $(BRANCH) --job-name $(JOB_NAME) "$(ARGS)"
