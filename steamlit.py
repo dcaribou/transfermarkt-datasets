d6e5160c896b2e843f77a83ec222b0daf5b359dc
dcaribou <davidcereijo@live.com>
2022-04-28 20:11:53 +0200

    testing

---
+++ steamlit.py
@@ -0,0 +1,13 @@
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
+import streamlit as st
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
