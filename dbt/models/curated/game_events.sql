638cfd6cc54fbc17104280f1c759846a3093d24a
dcaribou <davidcereijo@live.com>
2023-09-29 18:36:25 +0200

    add date field

--- dbt/models/curated/game_events.sql
+++ dbt/models/curated/game_events.sql
@@ -2,6 +2,11 @@ with game_events_cte as (
 
     select * from {{ ref('base_game_events') }}
 
+),
+games_cte as (
+
+    select * from {{ ref('base_games') }}
+
 )
 
 select
@@ -15,8 +20,11 @@ select
         'player_in_id',
         'player_assist_id'
     ]) }} as game_event_id,
-    * 
+    games_cte."date",
+    game_events_cte.* 
 
 from game_events_cte
+
+left join games_cte using(game_id)
 
 order by game_id, player_id, club_id, "type", "minute", coalesce("description", ''), player_in_id, player_assist_id
