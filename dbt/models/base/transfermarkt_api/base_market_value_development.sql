0b957e16efcaddef57d061d88fbdd76d522c3754
David <davidcereijo@live.com>
2023-12-05 17:29:59 +0100

    unnest market values

--- dbt/models/base/transfermarkt_api/base_market_value_development.sql
+++ dbt/models/base/transfermarkt_api/base_market_value_development.sql
@@ -5,19 +5,55 @@ with
             str_split(filename, '/')[5] as season,
             json(value) as json_row,
             json_extract_string(json_row, '$.player_id')::integer as player_id,
-            row_number() over (partition by player_id order by season desc) as n
+            row_number() over (partition by player_id order by season desc) as n,
+            filename
         
         from {{ source("transfermarkt_api", "market_value_development") }}
         
 
+    ),
+    unnested as (
+
+        select
+            unnest(json_transform(json_extract(json_row, '$.response.list'), '["JSON"]')) as json_row,
+            player_id,
+            season,
+            filename
+
+        from json_market_values
+
+        where n = 1 -- take the last season extraction for each player
+
+    ),
+    -- remove duplicates in case multiple market values appear for the same player in the same day
+    -- (it can happen)
+    deduplicated as (
+
+        select * from (
+
+            select
+                *,
+                strptime(json_row ->> 'datum_mw', '%b %d, %Y') as "datetime",
+                row_number() over (partition by player_id, "datetime"::date order by "datetime" desc) as n
+
+            from unnested
+            -- we need to remove the rows with no date information
+            where (json_row ->> 'datum_mw')::varchar != '-'
+
+        )
+        where 
+            n = 1 and
+            -- since we are at it, let's remove the rows with no market value information as well
+            (json_row ->> 'mw' != '-')
     )
 
 select
     player_id,
-    {{ parse_market_value("json_extract_string(json_row, '$.response.current')") }} as market_value_in_eur,
-    {{ parse_market_value("json_extract_string(json_row, '$.response.highest')") }} as highest_market_value_in_eur,
-    json_row
-
-from json_market_values
-
-where n = 1
+    {{ parse_market_value("json_row ->> 'mw'") }} as market_value_in_eur,
+    "datetime",
+    ("datetime")::date as "date",
+    date_trunc('week', "datetime") as dateweek,
+    season as last_season,
+    filename
+    
+from deduplicated
--- dbt/models/curated/players.sql
+++ dbt/models/curated/players.sql
@@ -10,18 +10,35 @@ clubs_cte as (
 ),
 players_market_value_cte as (
 
-    select * from {{ ref('base_market_value_development') }}
+    select
+        *,
+        row_number() over (partition by player_id order by date desc) as rn
 
+    from {{ ref('base_market_value_development') }}
+
+),
+-- get the current and maximum market value for each player
+market_value_agg_cte as (
+
+    select
+        player_id,
+        max(market_value_in_eur) as highest_market_value_in_eur,
+        max(case when rn = 1 then market_value_in_eur else null end) as market_value_in_eur
+
+    from players_market_value_cte
+
+    group by player_id
 )
 
 select
     players_cte.*,
     clubs_cte.domestic_competition_id as current_club_domestic_competition_id,
     clubs_cte.name as current_club_name,
-    players_market_value_cte.market_value_in_eur,
-    players_market_value_cte.highest_market_value_in_eur,
+    market_value_agg_cte.market_value_in_eur,
+    market_value_agg_cte.highest_market_value_in_eur,
 
 from players_cte
 
 left join clubs_cte on players_cte.current_club_id = clubs_cte.club_id
-left join players_market_value_cte on players_cte.player_id = players_market_value_cte.player_id
+
+left join market_value_agg_cte on players_cte.player_id = market_value_agg_cte.player_id
